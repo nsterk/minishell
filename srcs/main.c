7fa561e5a93255aad802c95e18eda2ff32cabe2c
Arthur Beznik <arthurbeznik@gmail.com>
2022-09-10 15:53:56 +0200

    test

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,12 @@
 /*   By: arthurbeznik <arthurbeznik@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/07 15:03:59 by arthurbezni   #+#    #+#                 */
-/*   Updated: 2022/09/07 15:06:37 by arthurbezni   ########   odam.nl         */
+/*   Updated: 2022/09/10 15:53:37 by arthurbezni   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(int argc, char **argv)
 {
-	printf("What a main.");
+	printf("What main.");
 	return (0);
 }
