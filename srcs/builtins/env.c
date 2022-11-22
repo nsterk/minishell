548a69820419f874df102444dc899e0c397b55af
abeznik <abeznik@student.codam.nl>
2022-11-22 12:26:07 +0100

    [ADD] env almost done

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: abeznik <abeznik@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/22 12:05:50 by abeznik       #+#    #+#                 */
-/*   Updated: 2022/11/22 12:20:40 by abeznik       ########   odam.nl         */
+/*   Updated: 2022/11/22 12:25:54 by abeznik       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 int	exec_env(char **envp)
 {
-	char	*output;
 	int		i;
 
 	i = 0;
 	while (envp[i])
 	{
-		output = getenv(envp[i]);
-		ft_putendl_fd(output, STDOUT_FILENO);
-		// free(output);
+		ft_putendl_fd(envp[i], STDOUT_FILENO);
 		i++;
 	}
 }
