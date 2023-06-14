acf7459ece10a4b46533f78eba65601a162d8046
Naomi Sterk <naomisterk@gmail.com>
2023-06-14 14:37:33 +0200

    sync

--- srcs/expander/expand_utils.c
+++ srcs/expander/expand_utils.c
@@ -2,14 +2,6 @@
 #include "minishell.h"
 
 static void	st_get_param(t_token *token, t_expansion *exp, size_t *pos);
-// static char *st_env_not_found(t_token *token, size_t start, size_t end);
-
-
-// OK so if token->word starts with a non-existing env var the 'front' part should not be allocated
-// cause then the new word will be \0 instead of NULL and that's a --> problem <--
-
-// NO it's not that we allocate and then put \0 there, it's that we increment pos and now it's pointing to the null terminator. so we need to delete the node.
-// so do token_remove
 
 bool	do_expanding(t_token *token, t_expansion *exp, size_t *pos, char **envp)
 {
